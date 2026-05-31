c61ffa01c09c4cf38d4988ee0b76537304042b6a
Thanus Thapasvi <t4872188@gmail.com>
2026-05-31 13:03:26 +0530

    Commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,23 +6,25 @@ function About() {
     const navigate = useNavigate();
     return (
         <section className='about-section'>
-            <h1>About me</h1>
+            <h1 className='page-heading'>About me</h1>
             <div className='section-content'>
                 <div className="about-info">
                     <h3 className='content-greeting'>Hi, I'm <span className='grad-text'>Thanus Thapasvi Thummala</span></h3>
                     <h2 className='content-role'>Fullstack Web <span>Developer</span></h2>
                     <p>I am a passionate fullstack web developer and I love creating modern web applications that are optimized for performance and good user experience with stunning design.</p>
                     <div className='button-box'>
-                        <button onClick={() => navigate('/projects')} className='btn'>
+                        <button onClick={() => navigate('/projects')} className='btn btn-primary'>
                             Projects
                         </button>
 
-                        <button onClick={() => navigate('/contact')} className='btn'>
+                        <button onClick={() => navigate('/contact')} className='btn btn-secondary'>
                             Contact me
                         </button>
                     </div>
                 </div>
                 <div className="profile-cover flex-center shining">
+                    <div className='profile-img-cover'></div>
+                    <div className='profile-img-cover'></div>
                     <img src={Profile} alt="profile pic" className='profile' />
                 </div>
             </div>
